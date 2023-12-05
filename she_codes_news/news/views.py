faf24c7a6e0e8518173e7be7185fd97a1bce231c
cristianetanaka <cristianeoshirotanaka@gmail.com>
2023-12-05 19:49:56 +0800

    django project 1

--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -12,5 +12,18 @@ class IndexView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        #context['latest_stories'] = NewsStory.objects.all().order_by('-pub_date')[:4]
+        #context['latest_stories'] = NewsStory.objects.all().order_by('-pub_date')[:4]
         context['latest_stories'] = NewsStory.objects.all()[:4]
         return context
+
+class StoryView(generic.DetailView):
+    model = NewsStory
+    template_name = 'news/story.html'
+    context_object_name = 'story'
+   
+#class AddStoryView(generic.CreateView):
+    #form_class = Storyform
+    #context_object_name = 'storyform'
+    #template_name = 'news/createStory.html'
+    #success_url = reverse_lazy('news:index')
