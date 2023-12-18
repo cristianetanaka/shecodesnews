2221da6371f13ad7318be0eccc429ee3c39a8b07
cristianetanaka <cristianeoshirotanaka@gmail.com>
2023-12-18 21:03:40 +0800

    fix url

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views import View
 from django.contrib import messages
-from .forms import CustomUserCreationForm, CustomUserChangeForm, UserProfileForm 
+from .forms import CustomUserCreationForm, CustomUserChangeForm, UserProfile 
 
 
 class CreateAccountView(CreateView):
@@ -15,29 +15,29 @@ class CreateAccountView(CreateView):
     def form_valid(self, form):
         #form.instance.author = self.request.user
         response = super() .form_valid(form)
-        UserProfileForm.objects.create(user=self.object)
+        UserProfile.objects.create(user=self.object)
         return response
 
 
 @login_required # user must be login to access view
               
 def profileView(request):
-    user_profile = UserProfileForm.objects.get(user=request.user)
-    return render(request, 'profile.html', {'user_profile' : user_profile})
+    user_profile = UserProfile.objects.get(user=request.user)
+    return render(request, 'users/profile.html', {'user_profile' : user_profile})
 
 @login_required
 
 class UpdateProfileView(View):
-    template_name = 'updateprofile.html'
+    template_name = 'users/updateprofile.html'
 
     def get(self, request, *args, **kwargs):
         user_form = CustomUserChangeForm(instance=request.user)
-        profile_form = UserProfileForm(instance=request.user.userprofile)
+        profile_form = UserProfile(instance=request.user.userprofile)
         return render(request, self.template_name, {'user_form': user_form, 'profile_form': profile_form})
 
     def post(self, request, *args, **kwargs):
         user_form = CustomUserChangeForm(request.POST, instance=request.user)
-        profile_form = UserProfileForm(request.POST, instance=request.user.userprofile)
+        profile_form = UserProfile(request.POST, instance=request.user.userprofile)
 
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
